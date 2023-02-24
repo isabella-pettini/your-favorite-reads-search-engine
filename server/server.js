ef02742afd4eee4f682ac821f06f8279e0d0e644
Isabella <isabellapierce93@gmail.com>
2023-02-23 17:51:48 -0700

    Refactor code in server - server.js/connection.js & client - app.js

--- server/server.js
+++ server/server.js
@@ -15,8 +15,6 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -25,13 +23,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+ }
+
+ startApolloServer(typeDefs, resolvers);
